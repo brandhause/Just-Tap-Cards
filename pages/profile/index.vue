1659407444b7d147a295979d92c2477c868dad00
Ryan Sutana <ryan@brandhause.com>
2023-03-02 10:00:54 +0800

    dashboard update

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,32 +1,40 @@
-<template>
+s<template>
   <div v-if="currentUser">
     <div class="container my-5">
       <div class="row justify-content-center">
-        <div class="col-md-5">
+        <div class="col-md-6">
           <div>
             <nuxt-link to="/">Home</nuxt-link>
           </div>
-          Hello {{ currentUser.displayName }}!
+          <div class="welcome-text">
+            <h2>Hello {{ fullName[0] }}!</h2>
+            <p>This is your profile where you can make changes anytime. Enjoy making new connections!</p>
+          </div>
+          
+          
           <div class="profile-details">
-            <div class="profile-img mx-auto" style="width: 100%; height: auto">
+            <nuxt-link class="edit-profile-link" to="/profile/edit-card-details"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg></nuxt-link>
+            <div class="profile-img mx-auto">
               <img :src="currentUser.profileImage" />
             </div>
             <div class="profile-info">
-              <h2>{{ currentUser.displayName }}</h2>
-              <p>
+              <h2>
+                {{ fullName[0] }}
+                <br />
+                {{ fullName[1] }}
                 <span class="job-title">{{ currentUser.jobTitle }}</span>
                 <span v-if="currentUser.company"> At {{ currentUser.company }}</span>
-                </p>
+              </h2>
             </div>
-          </div>
-          <div>
-            <nuxt-link to="/profile/edit-card-details">Edit profile</nuxt-link>
           </div>
           <div class="user-bio">
             <UserBio :currentUser="currentUser" />
           </div>
           <div class="social-media">
             <UserSocialMedia />
+          </div>
+          <div class="video">
+            <div ref="playerContainer"></div>
           </div>
           <div>
             <button @click="logout">Logout</button>
@@ -39,9 +47,14 @@
 <script setup>
 import { onAuthStateChanged } from 'firebase/auth';
 import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { ref, onMounted } from "vue";
 
   const currentUser = ref();
   const errCode = ref();
+
+  const fullName = computed(() => {
+    return currentUser.value.displayName.split(' ');
+  });
 
   onMounted(async () => {
     const { auth, firestore } = useFirebase();
@@ -59,6 +72,7 @@ import { doc, onSnapshot, updateDoc } from "firebase/firestore";
               uid: user.uid,
               ...snap.data()
             }
+            
           },
           (error) => {
             //
@@ -66,6 +80,10 @@ import { doc, onSnapshot, updateDoc } from "firebase/firestore";
         );
       }
     });
+
+    // userFirstName = fullName.value[0];
+    // userLastName = fullName.value[1];
+
   })
 
   async function logout() {
