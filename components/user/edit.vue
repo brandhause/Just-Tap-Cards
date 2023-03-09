d9a1ecc3dde8e06a356fed89916ccfe27c73ec4d
Ryan Sutana <ryan@brandhause.com>
2023-03-09 16:25:20 +0800

    update cropper js

--- components/user/edit.vue
+++ components/user/edit.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <form @submit.prevent="edit(user)">
-      <ImageUpload :uploadImage="uploadImage" />
+			<div class="crop-container mx-auto" style="max-height: 300px max-width: 300px">
+				<img class="mb-5" ref="cropper" src="https://picsum.photos/200">
+			</div>
+			<button @click="cropImage">Crop</button>
       <div>
         <label for="first-name">First name:</label>
         <input type="text" id="first-name" name="first-name" class="form-control mb-3" v-model="user.fname" />
@@ -27,6 +30,7 @@
   </div>
 </template>
 <script setup>
+import Cropper from 'cropperjs';
 
   const props = defineProps({
     edit: Function,
@@ -34,6 +38,32 @@
   });
 
   const user = ref({});
+  let cropper = ref();
+
+	function getRoundedCanvas(sourceCanvas) {
+      var canvas = document.createElement('canvas');
+      var context = canvas.getContext('2d');
+      var width = sourceCanvas.width;
+      var height = sourceCanvas.height;
+
+      canvas.width = width;
+      canvas.height = height;
+      context.imageSmoothingEnabled = true;
+      context.drawImage(sourceCanvas, 0, 0, width, height);
+      context.globalCompositeOperation = 'destination-in';
+      context.beginPath();
+      context.arc(width / 2, height / 2, Math.min(width, height) / 2, 0, 2 * Math.PI, true);
+      context.fill();
+      return canvas;
+  }
+
+  function cropImage() {
+		const canvas = cropper.getCroppedCanvas();
+		const roundedCanvas = getRoundedCanvas(canvas);
+
+      	const dataUrl = roundedCanvas.toDataURL('image/jpeg')
+      	console.log(dataUrl)
+	}
 
   const fullName = computed(() => {
     if (!props.currentUser && !props.currentUser.displayName) return [];
@@ -56,7 +86,40 @@
       jobTitle: props.currentUser.jobTitle,
       company: props.currentUser.company,
       categoryOfWork: props.currentUser.industryOrCategoryOfWork,
+      getImageUrl: props.currentUser.profileImage
     };
+
+    const image = cropper.value
+		let croppable = false;
+		cropper = new Cropper(image, {
+			dragMode: 'move',
+			zoomable: true,
+			minContainerWidth: 300,
+			minContainerHeight: 300,
+			minCropBoxWidth: 300,
+			minCropBoxHeight: 300,
+			cropBoxMovable: false,
+			guides: false,
+			center: true,
+			ready: function () {
+			croppable = true;
+			},
+			crop(event) {
+				console.log(event.detail.x)
+				console.log(event.detail.y)
+				console.log(event.detail.width)
+				console.log(event.detail.height)
+			}
+		})
   });
   
 </script>
+<style>
+.crop-container {
+	max-width: 300px;
+	max-height: 300px;
+}
+.cropper-hidden {
+    display: none !important;
+}
+</style>
