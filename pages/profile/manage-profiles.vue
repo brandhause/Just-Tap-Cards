de8379a91a29ec47636ae7de9471df42218409ad
ry <ryambao624@gmail.com>
2023-07-29 22:46:58 +0800

    Fix manage profile live card

--- pages/profile/manage-profiles.vue
+++ pages/profile/manage-profiles.vue
@@ -17,8 +17,8 @@
           <div>
             <small><strong>Live card now</strong></small>
           </div>
-          <select name="" id="" class="w-100 py-2 border-0 border-bottom">
-            <option value="" v-for="profile in currentUser.profile" :key="profile.id">
+          <select class="w-100 py-2 border-0 border-bottom" v-model="profileId">
+            <option :value="profile.id" v-for="profile in currentUser.profile" :key="profile.id">
               {{ profile.slug }}
             </option>
           </select>
@@ -26,12 +26,16 @@
             <div
               class="d-flex align-items-stretch position-relative"
               style="border-radius: 20px"
+              @click="changeProfile(profile.id)"
             >
               <div
                 class="profile-img mx-auto"
                 style="flex: 0 0 275px; height: 375px; border-radius: 20px 0 0 20px"
               >
-                <img :src="profile.profileImage" class="h-100 w-100" style="border-radius: 20px 0 0 20px" />
+                <img v-if="profile.profileImage" :src="profile.profileImage" class="h-100 w-100" style="border-radius: 20px 0 0 20px" />
+                <svg v-else width="100%" height="100%" viewBox="0 0 122 160" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path opacity="0.4" d="M61.0005 79C79.2248 79 94 61.3154 94 39.5002C94 17.6846 89.1491 0 61.0005 0C32.8518 0 28 17.6846 28 39.5002C28 61.3154 42.7752 79 61.0005 79Z" fill="#686868"></path><path opacity="0.4" d="M121.932 138.151C121.334 99.8308 116.405 88.9117 78.6861 82C78.6861 82 73.3766 88.8694 61.0014 88.8694C48.6261 88.8694 43.3157 82 43.3157 82C6.00888 88.8363 0.7801 99.5934 0.0925852 136.906C0.036209 139.953 0.0100836 140.113 0 139.759C0.00229172 140.422 0.0925852 160 0.0925852 160H61H122C122 160 121.999 141.457 122 140.807C121.99 141.026 121.97 140.602 121.932 138.151Z" fill="#686868"></path>
+                </svg>
               </div>
               <div
                 class="profile-info d-flex align-items-center text-white"
@@ -84,33 +88,39 @@
 <script setup>
 import { onAuthStateChanged } from 'firebase/auth';
 import { doc, onSnapshot, updateDoc, arrayRemove } from 'firebase/firestore';
-  
+import useFirestore from '~/composables/useFirestore.ts';
+
+  const { update } = useFirestore();  
   const nuxtApp = useNuxtApp();
   const currentUser = ref();
+  const liveProfile = ref();
+  const profileId = ref();
+
+  watch(() => profileId.value, (newVal, oldVal) => {
+    if (oldVal !== undefined && newVal !== oldVal) {
+      changeProfile(newVal)
+    }
+  })
 
   onMounted(async () => {
-    onAuthStateChanged(nuxtApp.$auth, (user) => {
-      if (!user) {
-        return navigateTo({
-          path: '/'
-        });
-      } else {
-        const docRef = doc(nuxtApp.$firestore, 'users', user.uid);
-
-        onSnapshot(docRef,
-          (snap) => {
-            currentUser.value = {
-              uid: user.uid,
-              ...snap.data()
-            }
-          },
-          (error) => {
-            //
-          },
-        );
-      }
-    });
+    currentUser.value = JSON.parse(localStorage.getItem('profiles'));
+    liveProfile.value = JSON.parse(localStorage.getItem('live-profile'));
+    profileId.value = liveProfile.value.id;
   })
+
+  async function changeProfile(id) {
+    const profile = currentUser.value;
+    profile.profile.forEach(async (p) => {
+      p.live = p.id === id;
+    })
+
+    await update(currentUser.value.uid, profile.profile)
+      .then((res) => {
+        if (res === 'ok') {
+          return navigateTo('/profile');
+        }
+      })
+  }
 
   async function removeProfile(profile) {
     try {
