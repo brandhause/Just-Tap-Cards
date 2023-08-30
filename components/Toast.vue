4609fb328fa944ba750888f6aaa6e3fc5bc9fb81
ry <ryambao624@gmail.com>
2023-08-30 12:13:22 +0800

    Added update email with validations

---
+++ components/Toast.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="alert-modal" v-if="alert.state" :class="alert.state">
+    <div class="alert-body d-flex px-3 py-3 rounded" :class="alert.state" style="gap: 16px">
+      <span>
+        <svg v-if="alert.state == 'success'" fill="#fff" width="24" height="24" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="m11.998 2.005c5.517 0 9.997 4.48 9.997 9.997 0 5.518-4.48 9.998-9.997 9.998-5.518 0-9.998-4.48-9.998-9.998 0-5.517 4.48-9.997 9.998-9.997zm-5.049 10.386 3.851 3.43c.142.128.321.19.499.19.202 0 .405-.081.552-.242l5.953-6.509c.131-.143.196-.323.196-.502 0-.41-.331-.747-.748-.747-.204 0-.405.082-.554.243l-5.453 5.962-3.298-2.938c-.144-.127-.321-.19-.499-.19-.415 0-.748.335-.748.746 0 .205.084.409.249.557z" fill-rule="nonzero" />
+        </svg>
+        <svg v-else width="24" height="24" fill="#fff" viewBox="0 0 24 24" focusable="false">
+          <path fill="currentColor" d="M11.983,0a12.206,12.206,0,0,0-8.51,3.653A11.8,11.8,0,0,0,0,12.207,11.779,11.779,0,0,0,11.8,24h.214A12.111,12.111,0,0,0,24,11.791h0A11.766,11.766,0,0,0,11.983,0ZM10.5,16.542a1.476,1.476,0,0,1,1.449-1.53h.027a1.527,1.527,0,0,1,1.523,1.47,1.475,1.475,0,0,1-1.449,1.53h-.027A1.529,1.529,0,0,1,10.5,16.542ZM11,12.5v-6a1,1,0,0,1,2,0v6a1,1,0,1,1-2,0Z"></path>
+        </svg>
+      </span>
+      <div class="text-white">
+        <div class="text-capitalize"><strong>{{ alert.title }}</strong></div>
+        <div>{{ alert.message }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+  const props = defineProps({
+    alert: {
+      type: Object
+    }
+  })
+</script>
+<style lang="scss" scoped>
+.alert-modal {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+  transition: all 0.5s ease;
+  opacity: 0;
+  pointer-events: none;
+
+  &.success,
+  &.error {
+    opacity: 1;
+  }
+
+  .alert-body {
+    width: 500px;
+
+    &.success {
+      background: #38A169;
+    }
+
+    &.error {
+      background: #e53e3e;
+    }
+
+    span {
+      color: #fff;
+    }
+  }
+}
+</style>
--- pages/my-account/change-email.vue
+++ pages/my-account/change-email.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="position-relative">
     <div class="row mx-0 col-12 px-0" style="color: #686868">
       <div class="col-12 px-0">
         <BackHeader />
@@ -11,22 +11,41 @@
       <div class="col-12 mt-4">
         <div class="d-flex flex-column">
           <small class="px-0" style="font-weight: 600">Current email</small>
-          <input class="border-0 border-bottom" style="cursor: not-allowed" type="text" :placeholder="currentUser.email" readonly />
+          <input class="border-0 border-bottom" style="cursor: not-allowed" type="text" :placeholder="currentUser.email" readonly disabled />
         </div>
       </div>
       <div class="col-12 mt-4 pt-3">
         <div class="d-flex flex-column">
-          <input class="border-0 border-bottom" type="text" placeholder="Current password" />
+          <input
+            class="border-0 border-bottom"
+            type="password"
+            placeholder="Current password"
+            v-model="user.providedPassword"
+            @blur="passwordErr"
+          />
+          <small>Password is required.</small>
         </div>
       </div>
       <div class="col-12 mt-4 pt-3">
         <div class="d-flex flex-column">
-          <input class="border-0 border-bottom" type="text" placeholder="New email" />
+          <input
+            class="border-0 border-bottom"
+            type="email"
+            placeholder="New email"
+            v-model="user.providedEmail"
+            @blur="emailErr"
+          />
+          <small>{{ emailError }}</small>
         </div>
       </div>
       <div class="col-12 mt-5 px-0">
         <div>
-          <button class="accent-btn border rounded px-5 py-2 w-100" disabled>
+          <button
+            class="accent-btn border rounded px-5 py-2 w-100"
+            :class="{'disabled': emailError || invalidate}"
+            :disabled="emailError || invalidate"
+            @click="reAuthenticate"
+          >
             <small>
               <strong>Change</strong>
             </small>
@@ -34,20 +53,105 @@
         </div>
       </div>
     </div>
+    <Toast :alert="alert" />
   </div>
 </template>
 <script setup>
-import { onAuthStateChanged } from 'firebase/auth';
-import { doc, onSnapshot } from "firebase/firestore";
+import { onAuthStateChanged, updateEmail, reauthenticateWithCredential, EmailAuthProvider } from 'firebase/auth';
 
   const nuxtApp = useNuxtApp();
   const currentUser = ref([]);
+  const user = ref({});
+  const alert = ref({});
+  const emailError = ref(null);
+  const invalidate = ref(true);
 
   onMounted(() => {
     onAuthStateChanged(nuxtApp.$auth, (user) => {
       currentUser.value = user;
+      
     });
   })
+
+  function passwordErr(e) {
+    const value = e.target.value;
+    if (!value) {
+      e.target.classList.add('invalid');
+      invalidate.value = true;
+    } else {
+      e.target.classList.remove('invalid');
+      invalidate.value = false;
+    }
+  }
+
+  function emailErr(e) {
+    const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const value = e.target.value;
+    if (!value) {
+      e.target.classList.add('invalid');
+      emailError.value = 'Email is required.'
+      invalidate.value = true;
+    } else if (!regEx.test(value)) {
+      e.target.classList.add('invalid');
+      emailError.value = 'Email format is wrong.'
+      invalidate.value = true;
+    } else {
+      e.target.classList.remove('invalid');
+      emailError.value = null;
+      invalidate.value = false;
+    }
+  }
+
+  function reAuthenticate() {
+    const authUser = nuxtApp.$auth.currentUser;
+    const credential = EmailAuthProvider.credential(
+      currentUser.value.email,
+      user.value.providedPassword
+    );
+    
+    reauthenticateWithCredential(authUser, credential)
+      .then((res) => {
+        // check email
+        if (JSON.stringify(res.user.email) === JSON.stringify(user.value.providedEmail)) {
+          alert.value.state = 'error';
+          alert.value.title = 'Something went Wrong!'
+          alert.value.message = 'The new email is the same as your current email!';
+          setTimeout(() => {
+            alert.value.state = null;
+          }, 3000);
+        } else if (user.value.providedEmail.match('\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b')) {
+          alert.value.state = 'error';
+          alert.value.title = 'Something went Wrong!'
+          alert.value.message = 'Invalid email!';
+          setTimeout(() => {
+            alert.value.state = null;
+          }, 3000);
+        } else {
+          // update email
+          updateEmail(authUser, user.value.providedEmail)
+            .then((res) => {
+              alert.value.state = 'success';
+              alert.value.title = 'success';
+              alert.value.message = 'Email successfully Updated!';
+            })
+            .catch((error) => {
+              alert.value.state = 'error';
+              alert.value.title = 'Something went Wrong!'
+              alert.value.message = error;
+              setTimeout(() => {
+                alert.value.state = null;
+              }, 3000);
+            })
+        }
+      }).catch((error) => {
+        alert.value.state = 'error';
+        alert.value.title = 'Something went Wrong!'
+        alert.value.message = error;
+        setTimeout(() => {
+          alert.value.state = null;
+        }, 3000);
+      });
+  }
 </script>
 <style lang="scss" scoped>
 .accent-btn {
@@ -56,6 +160,35 @@ import { doc, onSnapshot } from "firebase/firestore";
   color: #FFFFFF;
   &:hover {
     opacity: 1;
+  }
+  &.invalid {
+    background: #D4D4D4;
+  }
+}
+input {
+  &:focus {
+    outline: none;
+    border-color: #FF643A !important;
+    &::placeholder {
+      color: #FF643A;
+    }
+  }
+
+  ~ small {
+    display: none;
+    color: red;
+  }
+
+  &.invalid {
+    border-color: red !important;
+
+    &::placeholder {
+      color: red;
+    }
+
+    ~ small {
+      display: block;
+    }
   }
 }
 </style>
